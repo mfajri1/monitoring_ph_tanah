5d93789986fcf8a62bc4fdc8699004c3bfa2e54e
mfajri1 <53650514+mfajri1@users.noreply.github.com>
2025-07-31 21:09:21 +0700

    sada

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const cacheName = 'sensor-cache-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  'https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js',
+  'https://www.gstatic.com/firebasejs/8.10.1/firebase-database.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
+
